0271b51af486b149fa5cc9cd79429f78b9b717a8
nnkhuong <nguyenngockhuong@gmail.com>
2015-07-23 08:53:24 +0700

    commit id 12: added lesson3.lab3.prob1

---
+++ src/lesson3/lab3/prob1/PersonWithJob.java
@@ -0,0 +1,49 @@
+package lesson3.lab3.prob1;
+
+public class PersonWithJob {
+	
+	private double salary;
+	Person person;
+	
+	public double getSalary() {
+		return salary;
+	}
+	public Person getPerson() {
+		return person;
+	}
+	PersonWithJob(String n, double s) {
+		person = new Person(n);
+		salary = s;
+	}
+	
+	@Override
+	public boolean equals(Object aPerson) {
+		boolean isEqual;
+		if(aPerson == null) return false; 
+		if(aPerson instanceof PersonWithJob)
+		{
+			PersonWithJob p = (PersonWithJob)aPerson;
+			isEqual = this.person.equals(p.getPerson())  &&
+					this.getSalary()==p.getSalary();
+		}
+		else if(aPerson instanceof Person)
+		{
+			isEqual = this.person.equals(aPerson);
+		}
+		else
+		{
+			return false;
+		}
+		return isEqual;
+	}
+	public static void main(String[] args) {
+		PersonWithJob personWithJob = new PersonWithJob("Joe", 30000);
+		Person p1 = personWithJob.getPerson();
+		Person p2 = new Person("Joe");
+		//As PersonsWithJobs, p1 should be equal to p2
+		System.out.println("p1.equals(p2)? " + p1.equals(p2));
+		System.out.println("p2.equals(p1)? " + p2.equals(p1));
+	}
+
+
+}
